65f97702c587db7fd41df3a7f49c068b04821c76
giaunnps14677 <78287843+giaunnps14677@users.noreply.github.com>
2023-02-20 17:36:45 +0700

    Create FoodService.java

---
+++ src/main/java/edu/home/service/FoodService.java
@@ -0,0 +1,11 @@
+package edu.home.service;
+
+import java.util.List;
+
+import edu.home.common.entity.ImageNameFood;
+
+public interface FoodService {
+	    
+	 List<ImageNameFood> listfood(int id);
+	 
+}
