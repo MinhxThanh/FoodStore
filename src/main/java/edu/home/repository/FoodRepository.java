8e4cacda4c765607871e5dc7b714de0af2c68fb7
giaunnps14677 <78287843+giaunnps14677@users.noreply.github.com>
2023-02-20 21:44:54 +0700

    Update FoodRepository.java

--- src/main/java/edu/home/repository/FoodRepository.java
+++ src/main/java/edu/home/repository/FoodRepository.java
@@ -4,4 +4,6 @@ import edu.home.entity.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
+  @Query(value="{CALL PROCEDURE_NAME(:id)}", nativeQuery = true)
+	List<Tuple> procedure_name(@Param("id") Integer id);
 }
