ca8c90e4968e246146226346d5d89580e2b6b0c0
giaunnps14677 <78287843+giaunnps14677@users.noreply.github.com>
2023-03-17 21:35:49 +0700

    Update FoodServiceImpl.java

--- src/main/java/edu/home/service/impl/FoodServiceImpl.java
+++ src/main/java/edu/home/service/impl/FoodServiceImpl.java
@@ -16,10 +16,20 @@ import edu.home.common.entity.ImageNameFood;
 import edu.home.repository.FoodRepository;
 import edu.home.service.FoodService;
 
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.home.common.entity.FoodList;
+import edu.home.common.entity.ProcedureFoods;
+
 @Service
 public class FoodServiceImpl implements FoodService{
  @Autowired
  private FoodRepository dao;
+ @Autowired
+ private EntityManager em;
  
  @Override
  public List<ImageNameFood> listfood(int id){
@@ -50,5 +60,42 @@ public class FoodServiceImpl implements FoodService{
 			 t.get(3, String.class)
 	 )).collect(Collectors.toList());
 		return listFoods;
+	}
+	@Override
+	@SuppressWarnings("unchecked")
+	public List<FoodList> findAllFoodAndImageFromImageDescribes() {
+	    return em.createNamedStoredProcedureQuery("getFoodtList").getResultList();
+	}
+	
+	@Override
+	public List<FoodList> findAllFoodAndImageFromImageDescribesFunc() {
+	    List<Tuple> objects = dao.listFood();
+	    List<FoodList> list = objects.stream().map(t -> new FoodList(
+	            t.get(0, Integer.class),
+	            t.get(1, String.class),
+	            t.get(2, Double.class),
+	            t.get(3, String.class)
+	    )).collect(Collectors.toList());
+	    return list;
+	}
+
+	@Override
+	public Food findFoodByIdFood(Long id) {
+		return dao.findFoodByIdFood(id);
+	}
+	
+	@Override
+	public Food create(Food food) {
+	    return dao.save(food);
+	}
+	
+	@Override
+	public Food update(Food food) {
+	    return dao.save(food);
+	}
+	
+	@Override
+	public void delete(Long id) {
+	    dao.deleteById(id);
 	}
 }
