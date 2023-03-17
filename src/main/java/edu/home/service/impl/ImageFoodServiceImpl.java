e9f9a3a3f36a6a699f4116b906579f19ba3daf09
giaunnps14677 <78287843+giaunnps14677@users.noreply.github.com>
2023-03-17 21:39:18 +0700

    Update ImageFoodServiceImpl.java

--- src/main/java/edu/home/service/impl/ImageFoodServiceImpl.java
+++ src/main/java/edu/home/service/impl/ImageFoodServiceImpl.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import edu.home.entity.ImageFood;
+
 @Service
 public class ImageFoodServiceImpl implements ImageFoodService {
     @Autowired
@@ -18,4 +22,35 @@ public class ImageFoodServiceImpl implements ImageFoodService {
         System.out.println("searchByFoodId image");
         return dao.searchByFoodId(foodId);
     }
+    
+    @Override
+	public ImageFood save(ImageFood imageFood) {
+		
+		return dao.save(imageFood);
+	}
+
+	@Override
+	public ImageFood findById(Long id) {
+		return dao.findById(id).get();
+	}
+
+	@Override
+	public ImageFood update(ImageFood imageFood) {
+		return dao.save(imageFood);
+	}
+
+	@Override
+	public List<ImageFood> findByFoodId(Long id) {
+		return dao.findByFoodId(id);
+	}
+
+	@Override
+	public List<ImageFood> findAll() {
+		return dao.findAll();
+	}
+
+	@Override
+	public void delete(Long id) {
+		dao.deleteById(id);
+	}
 }
