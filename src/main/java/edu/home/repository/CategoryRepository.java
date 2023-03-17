5274b819a0e67234c2614384f80e751cff93e241
giaunnps14677 <78287843+giaunnps14677@users.noreply.github.com>
2023-03-17 21:08:27 +0700

    Update CategoryRepository.java

--- src/main/java/edu/home/repository/CategoryRepository.java
+++ src/main/java/edu/home/repository/CategoryRepository.java
@@ -3,5 +3,10 @@ package edu.home.repository;
 import edu.home.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Query;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+  @Query("select c from Category c join CategoryFood cf on cf.category.id = c.id " +
+            "join Food f on f.id = cf.food.id where f.id = ?1")
+    List<Category> findAllCategoriesByFoodId(Long id);
 }
