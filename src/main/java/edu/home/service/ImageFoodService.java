8763ec311679165ad3f41ac4b1c869bd9e4bf726
giaunnps14677 <78287843+giaunnps14677@users.noreply.github.com>
2023-03-17 21:20:34 +0700

    Update ImageFoodService.java

--- src/main/java/edu/home/service/ImageFoodService.java
+++ src/main/java/edu/home/service/ImageFoodService.java
@@ -6,4 +6,11 @@ import java.util.List;
 
 public interface ImageFoodService {
     List<ImageFood> searchByFoodId(Long foodId);
+    
+    ImageFood save(ImageFood imageFood);
+	ImageFood findById(Long id);
+	ImageFood update(ImageFood imageFood);
+	List<ImageFood> findByFoodId(Long id);
+	List<ImageFood> findAll();
+	void delete(Long id);
 }
