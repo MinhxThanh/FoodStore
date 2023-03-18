f7d6948190b33d11aa57fa9285d6f80896d8a0f5
giaunnps14677 <78287843+giaunnps14677@users.noreply.github.com>
2023-03-18 16:59:25 +0700

    Update and rename ImageFoodDto to ImageFoodDto.java

--- src/main/java/edu/home/common/entity/ImageFoodDto.java
+++ src/main/java/edu/home/common/entity/ImageFoodDto.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
