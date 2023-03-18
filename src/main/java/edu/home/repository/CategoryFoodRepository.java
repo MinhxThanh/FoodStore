5c33651fc313e1b0d4ebd63d60f7c67eac8c1400
giaunnps14677 <78287843+giaunnps14677@users.noreply.github.com>
2023-03-18 17:08:22 +0700

    Update CategoryFoodRepository.java

--- src/main/java/edu/home/repository/CategoryFoodRepository.java
+++ src/main/java/edu/home/repository/CategoryFoodRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-package edu.home.repository;
 
 import edu.home.common.entity.FoodByCategoryId;
 import org.springframework.data.jpa.repository.Modifying;
